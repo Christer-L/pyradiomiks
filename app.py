7c1b5ad064f8dbefc1934b255889115915d050ed
Christer-L <christer.lohk@gmail.com>
2023-11-27 17:53:30 +0100

    check problem with tables

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from arkitekt import register
 import time
-from mikro.api.schema import RepresentationFragment, from_xarray
+from mikro.api.schema import RepresentationFragment, from_xarray, from_df, TableFragment
 import numpy as np
 from skimage.filters import gaussian
+import pandas as pd
+
 
 @register
-def gaussian(image: RepresentationFragment, sigma: float) -> RepresentationFragment:
+def gaussian(image: RepresentationFragment) -> RepresentationFragment:
     """A simple gaussian filter
 
     This function applies gaussian filter to the imags.
@@ -21,7 +23,6 @@ def gaussian(image: RepresentationFragment, sigma: float) -> RepresentationFragm
     -------
     RepresentationFragment
         The blurred image
-
     """
     image_gaussian = gaussian(np.array(image.data), sigma=sigma)
     image_gaussian = np.array(image_gaussian)
@@ -30,4 +31,33 @@ def gaussian(image: RepresentationFragment, sigma: float) -> RepresentationFragm
     )
 
 
+@register
+def get_features(mask: RepresentationFragment, image: RepresentationFragment) -> TableFragment:
+    """Feature extractor    
+
+    Parameters
+    ----------
+    image : RepresentationFragment
+        The input image
+
+    sigma : Gaussian filter radius
+
+    Returns
+    -------
+    RepresentationFragment
+        The blurred image
+    """
+
+    df_list = []
+    unique_values, counts = np.unique(mask.data, return_counts=True)
+
+    for value, count in zip(unique_values, counts):
+        df_list.append([value, count])
+
+    df = pd.DataFrame(df_list, columns=["unique_value", "count"])
+    df_test = pd.DataFrame([[4,5], [4,5], [3, 2]], columns=["test1", "test2"])
+    print(df_test)
+    return from_df(
+        df_test, name="Counts_" + mask.name
+    )
 
